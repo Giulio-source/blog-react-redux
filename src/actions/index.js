2e7764e888cff23927557af3224f0e5b8a3da756
Net-Informatica <appnetinformatica@Mac-mini-di-Alessandro.local>
2021-03-26 17:58:01 +0100

    bad approach

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
-export const getPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const getPosts = async () => {
+  const response = await jsonPlaceholder.get("/posts");
   return {
     type: "GET_POSTS",
-    payload: "HELLO",
+    payload: response,
   };
 };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getPosts } from "../actions";
-import axios from "axios";
 
 function PostList() {
   const dispatch = useDispatch();
@@ -9,7 +8,6 @@ function PostList() {
   const postURL = "https://jsonplaceholder.typicode.com/posts";
 
   useEffect(() => {
-    const fetchedPosts = axios.get(postURL).then((data) => data.data);
     dispatch(getPosts());
   }, []);
   return <div>postlist</div>;
